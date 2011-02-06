360254cd42819a142fa20794980ae228137390a8
Philip Dorrell <http://www.1729.com/email.html>
2011-02-06 19:43:51 +1300

    list local directories

--- _project.el
+++ _project.el
@@ -1,5 +1,7 @@
 
 (load-this-project
  `( (:ruby-executable ,*ruby-1.9-executable*)
+    (:run-project-command (ruby-run-file ,(concat (project-base-directory) "RunMain.rb")))
+    (:ruby-args ("-I."))
     ) )
 
--- synqa.rb
+++ synqa.rb
@@ -1,2 +1,35 @@
 
-puts "Hello"
+class DirContentReader
+    
+  attr_reader :shell, :host, :pathPrefix, :hashCommand
+    
+  def initialize(hashCommand, shell, host, pathPrefix = "")
+    puts "DirContentReader.initialize , hashCommand = #{hashCommand} pathPrefix = #{pathPrefix}"
+    @hashCommand = hashCommand
+    @shell = shell
+    @host = host
+    @pathPrefix = pathPrefix
+  end
+  
+  def findDirectoriesCommand(baseDir)
+    return "#{@pathPrefix}find #{baseDir} -type d -print"
+  end
+  
+  def findDirectories(baseDir)
+    command = findDirectoriesCommand(baseDir)
+    puts "#{command} ..."
+    system({"CYGWIN" => "nodosfilewarning"}, command)
+  end
+end
+
+class CygwinLocalContentReader<DirContentReader
+  
+  def initialize(hashCommand, cygwinPath = "")
+    super(hashCommand, nil, nil, cygwinPath)
+    puts "pathPrefix = #{@pathPrefix}"
+  end
+
+end
+
+  
+  
