9c690c9a74395e0b15ad2285ada27d5533c31e06
Philip Dorrell <http://www.1729.com/email.html>
2011-02-06 20:27:07 +1300

    hash each file

--- synqa.rb
+++ synqa.rb
@@ -15,16 +15,14 @@ class DirContentReader
     return ["#{@pathPrefix}find.exe", baseDir, "-type", "d", "-print"]
   end
   
-  def findDirectoriesOutput(baseDir)
-    return getCommandOutput(findDirectoriesCommand(baseDir))
-  end
-  
   def listDirectories(baseDir)
-    output = findDirectoriesOutput(baseDir)
+    output = getCommandOutput(findDirectoriesCommand(baseDir))
     directories = []
     baseDirLen = baseDir.length
+    puts "Listing directories ..."
     while (line = output.gets)
       line = line.chomp
+      puts " #{line}"
       if line.start_with?(baseDir)
         directories << line[baseDirLen..-1]
       else
@@ -33,6 +31,30 @@ class DirContentReader
     end
     return directories
   end
+  
+  def findFilesCommand(baseDir)
+    return ["#{@pathPrefix}find.exe", baseDir, "-type", "f", "-print"]
+  end
+  
+  def listFileHashes(baseDir)
+    output = getCommandOutput(findFilesCommand(baseDir))
+    files = []
+    baseDirLen = baseDir.length
+    puts "Listing files ..."
+    while (line = output.gets)
+      filePath = line.chomp
+      puts " #{filePath}"
+      if line.start_with?(baseDir)
+        relativePath = filePath[baseDirLen..-1]
+        hash = getFileHash(filePath)
+        files << [relativePath, hash]
+      else
+        raise "File #{line} is not contained within base directory #{baseDir}"
+      end
+    end
+    return files
+  end
+    
 end
 
 class CygwinLocalContentReader<DirContentReader
@@ -40,6 +62,17 @@ class CygwinLocalContentReader<DirContentReader
   def initialize(hashCommand, cygwinPath = "")
     super(hashCommand, nil, nil, cygwinPath)
     puts "pathPrefix = #{@pathPrefix}"
+  end
+  
+  def getFileHash(filePath)
+    output = getCommandOutput([pathPrefix + hashCommand[0]] + hashCommand[1..-1] + [filePath])
+    puts "  hash of #{filePath}"
+    hashLine = nil
+    while (line = output.gets)
+      hashLine = line.chomp
+      puts "    #{hashLine}"
+    end
+    return hashLine
   end
 
   def getCommandOutput(command)
