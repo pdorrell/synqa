89dacff8af424c0a94709344c843bd778526bd1c
Philip Dorrell <http://www.1729.com/email.html>
2011-02-27 10:01:52 +1300

    add a passing test for HashCommand

--- test/test_synqa.rb
+++ test/test_synqa.rb
@@ -1,7 +1,17 @@
 require 'helper'
 
-class TestSynqa < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+module Synqa
+  class TestHashCommand < Test::Unit::TestCase
+    context "A hash command" do
+      setup do
+        @hashCommand = HashCommand.new("sha21", 16, 3)
+      end
+      
+      should "extract relative path and hash" do
+        pathWithHash = @hashCommand.parseFileHashLine("/home/dir/", "abcdeabcde012345   /home/dir/subdir/file.txt")
+        assert_equal "abcdeabcde012345", pathWithHash.hash
+        assert_equal "subdir/file.txt", pathWithHash.relativePath
+      end
+    end
   end
 end
