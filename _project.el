7ba0e24e77e24b1b606f72de8cd1e6c4ec0a64de
Philip Dorrell <http://www.1729.com/email.html>
2011-02-26 22:06:14 +1300

    updates for gem build

--- _project.el
+++ _project.el
@@ -2,6 +2,8 @@
 (load-this-project
  `( (:ruby-executable ,*ruby-1.9-executable*)
     (:run-project-command (ruby-run-file ,(concat (project-base-directory) "RunMain.rb")))
+    (:build-function project-compile-with-command)
+    (:compile-command "c:/Ruby192/bin/rake")
     (:ruby-args ("-I."))
     ) )
 
