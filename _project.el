b498d1a7401ff9642c1496d96cc0f35fb2212237
Philip Dorrell <http://www.1729.com/email.html>
2011-02-27 10:01:41 +1300

    compile command for Emacs is now "rake"

--- _project.el
+++ _project.el
@@ -3,7 +3,7 @@
  `( (:ruby-executable ,*ruby-1.9-executable*)
     (:run-project-command (ruby-run-file ,(concat (project-base-directory) "RunMain.rb")))
     (:build-function project-compile-with-command)
-    (:compile-command "c:/Ruby192/bin/rake")
+    (:compile-command "rake")
     (:ruby-args ("-I."))
     ) )
 
