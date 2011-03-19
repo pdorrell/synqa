78ad6d049f398243c179031346c4153949b27974
Philip Dorrell <http://www.1729.com/email.html>
2011-03-19 14:32:59 +1300

    test subdirs in base dir, and add extra test files

--- test/test_based_directory.rb
+++ test/test_based_directory.rb
@@ -18,9 +18,16 @@ module Based
       
       should "find files in top-level directory and get their names and relative paths" do
         names = @baseDir.files.map {|file| file.name}
-        assert_equal names, ["file1.txt", "file2.txt"]
+        assert_equal ["file1.txt", "file2.txt"], names
         relativePaths = @baseDir.files.map {|file| file.relativePath}
-        assert_equal relativePaths, ["file1.txt", "file2.txt"]
+        assert_equal ["file1.txt", "file2.txt"], relativePaths
+      end
+      
+      should "find sub-directories in top-level directory and get their names and relative paths" do
+        names = @baseDir.dirs.map {|dir| dir.name}
+        assert_equal ["dir2", "dir3"], names
+        relativePaths = @baseDir.dirs.map {|dir| dir.relativePath}
+        assert_equal ["dir2/", "dir3/"], relativePaths
       end
     end
   end
