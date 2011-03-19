45786775e4add6e201e6c6021a6f37c8637bceb2
Philip Dorrell <http://www.1729.com/email.html>
2011-03-19 16:44:27 +1300

    rdocs for based-directory

--- lib/based-directory.rb
+++ lib/based-directory.rb
@@ -11,18 +11,32 @@ module Based
   
   # A base class for directories: i.e. either the base directory itself, or a sub-directory
   class Directory
+    # The base directory (object)
     attr_reader :base
+    
+    # The path of this directory relative to the base directory (includes a following "/" if non-empty)
     attr_reader :relativePath
+    
+    # The elements of the relative path as an array
     attr_reader :pathElements
+    
+    # The immediate name of the directory (nil for the base directory)
     attr_reader :name
+    
+    # The parent directory (nil for the base directory)
     attr_reader :parent
+    
+    # The full path of the file
     attr_reader :fullPath
     
+    # initialise with un-initialised entries
     def initialize
       @entries = nil
-      @subDirs = nil
     end
     
+    # get the "entries", i.e. list of files and directories immediately contained in this directory, and cache them.
+    # Note that dirExclude, fileInclude and fileExclude functions in the base directory object
+    # may be applied to filter out some entries.
     def getEntries
       if @entries == nil
         @entries = Dir.entries(fullPath)
@@ -51,16 +65,19 @@ module Based
       end
     end
     
+    # Get list of files immediately contained in this directory
     def files
       getEntries()
       return @files
     end
     
+    # Get list of directories immediately contained in this directory
     def dirs
       getEntries()
       return @dirs
     end
     
+    # Get a list of all the sub-directories of this directory (with parents preceding children)
     def subDirs
       result = []
       for dir in dirs
@@ -70,6 +87,7 @@ module Based
       return result
     end
     
+    # Get a list of all files contained within this directory
     def allFiles
       result = files
       for subDir in subDirs
@@ -79,8 +97,10 @@ module Based
     end
   end
   
+  # An object representing a sub-directory (i.e. not the base directory itself)
   class SubDirectory<Directory
     
+    # Construct directory object from parent directory object and this directory's name
     def initialize(name, parent)
       super()
       @name = name
@@ -92,12 +112,21 @@ module Based
     end
   end
   
+  # An object representing the base directory
   class BaseDirectory<Directory
     
+    # Function to decide if files should be included (if nil, assume all included). Subject
+    # to exclusion by fileExclude
     attr_reader :fileInclude
+    
+    # Function to decide if files should be excluded
     attr_reader :fileExclude
+    
+    # Function to decide if sub-directories should be excluded (if a directory is excluded, so 
+    # are all it's sub-directories and files contained within)
     attr_reader :dirExclude
     
+    # Initialise from absolute file path. Options include :dirExclude, :fileInclude and :fileExclude
     def initialize(path, options = {})
       super()
       @name = nil
@@ -112,14 +141,27 @@ module Based
     end
   end
   
+  # An object representing a file within the base directory
   class File
+    # immediate name of file
     attr_reader :name
+    
+    # parent, i.e. containing directory
     attr_reader :parent
+    
+    # the base directory
     attr_reader :base
+    
+    # path of this file relative to base directory
     attr_reader :relativePath
+    
+    # elements of file path (including the name) as an array
     attr_reader :pathElements
+    
+    # full absolute path name of file
     attr_reader :fullPath
     
+    # initialise from name and containing directory
     def initialize(name, parent)
       super()
       @name = name
