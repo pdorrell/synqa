10be31e46ebde03e47acce6f00dcd749f33b69f7
Philip Dorrell <http://www.1729.com/email.html>
2011-04-17 16:55:58 +1200

    update examples for ExternalSshScp

--- examples/synqa-useage.rb
+++ examples/synqa-useage.rb
@@ -10,12 +10,14 @@ include Synqa
 sha256Sum = Sha256SumCommand.new()    # sha256sum (with 2 characters between hash and file name)
 sha256 = Sha256Command.new()          # sha256 -r (with 1 space between hash and file name)
 
+plinkAndPscp = ExternalSshScp.new("plink", "pscp")
+
 localContentLocation = LocalContentLocation.new(Based::BaseDirectory.new("c:/dev/src/project"), 
                                                 Digest::SHA256, 
                                                 "c:/temp/synqa/local.project.content.cache.txt")
 
 remoteHost = SshContentHost.new("username@host.example.com", 
-                                sha256, "plink", "pscp")
+                                sha256, plinkAndPscp)
 
 # Note: the specification of plink & pscp assumes that keys are managed with Pageant, and therefore
 # do not need to be specified on the command line.
